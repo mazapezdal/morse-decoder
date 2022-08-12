b7243c594401f0f6fe2a69742821101709bc05c4
Vladislav Mazannik <mazanniknnik@gmail.com>
2022-08-12 16:53:53 +0300

    feat: start/end morse

--- src/index.js
+++ src/index.js
@@ -1,44 +1,65 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
 };
 
 function decode(expr) {
-    // write your solution here
+
+    let arraySplitExpr = expr.match(/.{1,10}/g);
+
+    let decodeSplitExpr = [];
+    for (i = 0; i < arraySplitExpr.length; i++) {
+        decodeSplitExpr.push(arraySplitExpr[i].replace(/00/g, '').replace(/10/g, '.').replace(/11/g, '-'));
+    }
+
+    let decodeArrayOneMoreTime = [];
+    for (i = 0; i < decodeSplitExpr.length; i++) {
+        if (decodeSplitExpr[i] == '**********') {
+            decodeArrayOneMoreTime.push(decodeSplitExpr[i] = ' ');
+        } else {
+            for (let key in MORSE_TABLE) {
+                if (decodeSplitExpr[i] === key) {
+                    decodeArrayOneMoreTime.push(MORSE_TABLE[key]);
+                }
+            }
+        }
+    }
+   
+return decodeArrayOneMoreTime.join('');
 }
 
 module.exports = {
